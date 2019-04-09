702d865f7fe546dc053a6ec6a8547f05a9288b9e
Kat Maldon <katharinamaldon@gmail.com>
2019-04-09 16:31:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,9 +56,9 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  # return an array of holiday names (as symbols) where supply lists include the string "BBQ"
+  holiday_hash.values.select do |key, value| 
+    if value == "BBQ"
 end
 
 
